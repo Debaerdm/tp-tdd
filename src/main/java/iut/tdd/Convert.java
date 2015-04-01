80014b3f5ba02bf722c48a2f3cfb27cfb25790f4
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-04-01 09:19:26 +0200

    Tout les chiffres de 0 à 9 fonctionne

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,10 +2,10 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-		String[] text = new String[]{"zéro"};
-		String[] num = new String[]{"0"};
+		String[] text = new String[]{"zéro","un","deux","trois","quatre","cinq","six","sept","huit","neuf"};
+		String[] num = new String[]{"0","1","2","3","4","5","6","7","8","9"};
 		
-		for (int i = 0; i < num.length; i++) {
+		for (int i = 0; i < text.length; i++) {
 			if (num[i].equals(input)) {
 				return text[i];
 			}
