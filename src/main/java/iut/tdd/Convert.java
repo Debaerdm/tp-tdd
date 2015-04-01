fcb4377544fcd7381008a8fdb52cb9b1da590490
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-04-01 08:50:18 +0200

    Test de conversion au vert

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -2,14 +2,14 @@ package iut.tdd;
 
 public class Convert {
 	public static String num2text(String input) {
-//		String[] text = new String[]{"zéro"};
-//		String[] num = new String[]{"0"};
-//		
-//		for (int i = 0; i < num.length; i++) {
-//			if (num[i].equals(input)) {
-//				return text[i];
-//			}
-//		}
+		String[] text = new String[]{"zéro"};
+		String[] num = new String[]{"0"};
+		
+		for (int i = 0; i < num.length; i++) {
+			if (num[i].equals(input)) {
+				return text[i];
+			}
+		}
 		return null;
 	}
 	public static String text2num(String input) {
