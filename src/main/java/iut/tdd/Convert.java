5c65c47facf04784caae4c9d7a89bc1018f4ac2b
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-04-01 08:49:12 +0200

    Test de conversion en rouge

---
+++ src/main/java/iut/tdd/Convert.java
@@ -0,0 +1,18 @@
+package iut.tdd;
+
+public class Convert {
+	public static String num2text(String input) {
+//		String[] text = new String[]{"zéro"};
+//		String[] num = new String[]{"0"};
+//		
+//		for (int i = 0; i < num.length; i++) {
+//			if (num[i].equals(input)) {
+//				return text[i];
+//			}
+//		}
+		return null;
+	}
+	public static String text2num(String input) {
+		return null;
+	}
+}
